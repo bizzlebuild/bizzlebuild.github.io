e9b588b62d7ac252ca4a54c1dddd6bdbf5477f65
Bizzle Build <bizzlecreations@gmail.com>
2025-08-30 17:19:59 +0100

    getting there

---
+++ plugin.audio.streaming-radio-master/addon.py
@@ -0,0 +1,261 @@
+import os
+import xml.etree.ElementTree as et
+import urlparse
+import sys
+import requests
+import HTMLParser
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+
+import xbmc
+import xbmcgui
+import xbmcaddon
+import xbmcplugin
+from resources.lib import skinpatch
+
+plugin_url = sys.argv[0]
+handle = int(sys.argv[1])
+addon = xbmcaddon.Addon()
+
+sources_path = os.path.join(addon.getAddonInfo("path"), "sources")
+
+
+class RadioSource():
+    def __init__(self, file):
+        # Load XML
+        xml = et.parse(os.path.join(sources_path, file + ".xml")).getroot()
+
+        # Load source properties from XML
+        self.name = xml.find("name").text
+        self.streams = dict((int(stream.get("bitrate", default=0)), stream.text)
+                            for stream in xml.findall("stream"))
+        self.info = dict((child.tag, child.text) for child in xml
+                         if child.tag not in ("name", "stream", "scraper"))
+        self.url = "{0}?source={1}".format(plugin_url, file)
+
+        # Load scraper properties
+        if xml.find("scraper") is not None:
+            self.scraper = dict((child.tag, child.text) for child in xml.find("scraper"))
+            self.scraper["type"] = xml.find("scraper").get("type", default=None)
+        else:
+            self.scraper = None
+
+    # Generate a Kodi list item from the radio source
+    def list_item(self):
+        li = xbmcgui.ListItem(self.name, iconImage="DefaultAudio.png")
+        li.setInfo("music", {"title": self.name,
+                             "artist": self.info.get("tagline", None),
+                             "genre": self.info.get("genre", None)
+                             })
+        li.setArt(self._build_art())
+        return li
+
+    # Start playing the radio source
+    def play(self):
+        # Detect correct bitrate stream to play
+        if addon.getSetting("bitrate") == "Maximum":
+            self.stream_url = self.streams[max(self.streams.keys())]
+        else:
+            max_bitrate = int(addon.getSetting("bitrate").split(" ")[0])
+            bitrates = [bitrate for bitrate in self.streams.keys() if bitrate <= max_bitrate]
+            self.stream_url = (self.streams[min(self.streams.keys())]
+                               if len(bitrates) == 0 else self.streams[max(bitrates)])
+
+        # Create list item with stream URL and send to Kodi
+        li = self.list_item()
+        li.setPath(self.stream_url)
+        RadioPlayer().play_stream(self)
+
+    # Create dictionary of available artwork files to supply to list item
+    def _build_art(self):
+        art = {}
+        for art_type in ("thumb", "fanart", "poster", "banner",
+                         "clearart", "clearlogo", "landscape", "icon"):
+            if self.info.get(art_type, None) is not None:
+                path = os.path.join(addon.getAddonInfo("path"), "artwork", self.info[art_type])
+                if os.path.isfile(path):
+                    art[art_type] = path
+        return art
+
+
+class RadioPlayer(xbmc.Player):
+    def __init__(self):
+        xbmc.Player.__init__(self)
+
+    def play_stream(self, source):
+        self.play(item=source.stream_url, listitem=source.list_item())
+
+        if source.scraper is not None:
+            info = RadioInfo(source)
+            start_time = datetime.today()
+            # Wait for playback to start, then loop until stopped
+            while (self.isPlayingAudio() and xbmc.Player().getPlayingFile() == source.stream_url
+                   or datetime.today() <= start_time + timedelta(seconds=5)):
+                info.update()
+                xbmc.sleep(1000)
+
+            info.cleanup()  # Remove window properties on playback stop
+
+
+class RadioInfo():
+    def __init__(self, source):
+        self.api_key = requests.get("https://dev.rocketchilli.com/"
+                                    "keystore/ba7000f9-7ef4-4ace-b"
+                                    "ca2-f527cdffb393").json()["api-key"]
+        self.window = xbmcgui.Window(10000)  # Attach properties to the home window
+        self.window_properties = []
+
+        self.scraper = source.scraper
+        self.info = {"station": source.name}
+        self.first_update = True
+        self.next_update = datetime.today()
+        self.delayed = False
+
+    def update(self):
+        if self.next_update <= datetime.today():
+            changed = self.get_now_playing()
+            # Get track info if track has changed
+            if changed:
+                self.get_track_info()
+            # Apply delay so OSD update if required
+            if changed and "delay" in self.scraper and not self.delayed and not self.first_update:
+                self.next_update = datetime.today() + timedelta(seconds=int(self.scraper["delay"]))
+                self.delayed = True
+            # Set track info if no delay is required, or if a delay has already been applied
+            elif changed or self.delayed:
+                self.set_info()
+                self.delayed = self.first_update = False
+            # Wait as usual if track has not changed
+            if not self.delayed:
+                self.next_update = datetime.today() + timedelta(seconds=10)
+
+    # Push track info to the skin as window properties
+    def set_info(self):
+        for name, value in self.info.items():
+            name = addon.getAddonInfo("id") + "." + name
+            if value is None:
+                self.window.clearProperty(name)
+                if name in self.window_properties:
+                    self.window_properties.remove(name)
+            else:
+                self.window.setProperty(name, value)
+                if name not in self.window_properties:
+                    self.window_properties.append(name)
+
+    def cleanup(self):
+        for name in self.window_properties:
+            self.window.clearProperty(name)
+
+    def get_now_playing(self):
+        track_id = self.id_track()
+        if self.scraper["type"] == "tunein":
+            self._update_tunein()
+        # Return True if track info has changed
+        return track_id != self.id_track()
+
+    # Retrieve additional track info from last.fm API
+    def get_track_info(self):
+        # Reset track information before updating
+        for key, value in self.info.items():
+            if key not in ("title", "artist", "station"):
+                self.info[key] = None
+
+        # Request track information
+        track_url = ("http://ws.audioscrobbler.com/2.0/?method=track.getInfo"
+                     "&api_key={0}&artist={1}&track={2}&format=json")
+        try:
+            response = requests.get(track_url.format(self.api_key, urlencode(self.info["artist"]),
+                                                     urlencode(self.info["title"])))
+            if response.status_code == requests.codes.ok and "track" in response.json():
+                track_info = response.json()["track"]
+                if "album" in track_info:
+                    self.info["album"] = track_info["album"]["title"]
+                    if "image" in track_info["album"] and len(track_info["album"]["image"]) > 0:
+                        self.info["thumb"] = track_info["album"]["image"][-1]["#text"]
+                if "duration" in track_info:
+                    self.duration = int(track_info["duration"])
+                if "toptags" in track_info and len(track_info["toptags"]["tag"]) > 0:
+                    self.info["genre"] = track_info["toptags"]["tag"][0]["name"].capitalize()
+        except requests.exceptions.ConnectionError:
+            pass
+
+    def id_track(self):
+        return self.info.get("title", "") + self.info.get("artist", "")
+
+    # Scrape track info from Tunein website
+    def _update_tunein(self):
+        try:
+            html = BeautifulSoup(requests.get(self.scraper["url"]).text, "html.parser")
+            match = html.select("p.guide-item__guideItemSubtitle___2hQxF")
+
+            if len(match) > 0 and " - " in match[0].string:
+                self.info["artist"], self.info["title"] = match[0].string.split(" - ")
+        except requests.exceptions.ConnectionError:
+            pass
+
+
+# Build a list of radio stations in the Kodi GUI
+def build_list():
+    source_list = [RadioSource(file) for file in sources]
+    # Sort sources by XML <sort> property and then name
+    source_list = sorted(source_list, key=lambda s: s.name)
+    source_list = sorted(source_list, key=lambda s: float(s.info.get("sort", "Inf")))
+
+    # Create list item for each source XML entry
+    for source in source_list:
+        li = source.list_item()
+        xbmcplugin.addDirectoryItem(handle=handle, url=source.url, listitem=li, isFolder=False)
+
+    xbmcplugin.addSortMethod(handle, xbmcplugin.SORT_METHOD_UNSORTED)
+    xbmcplugin.addSortMethod(handle, xbmcplugin.SORT_METHOD_TITLE)
+    xbmcplugin.addSortMethod(handle, xbmcplugin.SORT_METHOD_GENRE)
+    xbmcplugin.endOfDirectory(handle)
+
+
+def unescape(string):
+    html_parser = HTMLParser.HTMLParser()
+    return html_parser.unescape(string)
+
+
+def urlencode(string):
+    return requests.utils.quote(string.encode("utf8"))
+
+
+# Request permission from user to modify skin files
+def prompt_skinpatch():
+    if addon.getSetting("skin-patch-prompt") == "true":
+        if xbmcgui.Dialog().yesno(heading=addon.getAddonInfo("name"),
+                                  line1=addon.getLocalizedString(30003),
+                                  line2=addon.getLocalizedString(30004)):
+            addon.setSetting("skin-patch", "true")
+        addon.setSetting("skin-patch-prompt", "false")
+    # Patch skin to allow track info to be displayed
+    if addon.getSetting("skin-patch") == "true":
+        skinpatch.SkinPatch().autopatch()
+
+
+# Extract URL parameters
+params = dict((key, value_list[0]) for key, value_list
+              in urlparse.parse_qs(sys.argv[2][1:]).items())
+
+# Load source filenames into list
+sources = []
+for source in os.listdir(sources_path):
+    source_file = os.path.join(sources_path, source)
+    if os.path.isfile(source_file):
+        name, extension = os.path.splitext(source)
+        if extension == ".xml":
+            sources.append(name)
+
+# Remove all skin patches
+if params.get("action", None) == "unpatch":
+    skinpatch.autoremove()
+    addon.setSetting("skin-patch", "false")
+    addon.setSetting("skin-patch-prompt", "true")
+else:
+    # Run addon
+    if params.get("source", None) is None:
+        build_list()
+    else:
+        prompt_skinpatch()
+        RadioSource(params["source"]).play()
---
+++ repo/plugin.audio.streaming-radio-master/resources/lib/skinpatch.py
@@ -0,0 +1,152 @@
+import os
+import xml.etree.ElementTree as et
+import shutil
+
+import xbmc
+import xbmcgui
+import xbmcaddon
+import patch
+
+STATUS_CLEAN = 0
+STATUS_PATCHED = 1
+STATUS_UPDATED = 2  # The addon has been updated since patching
+
+data_path = xbmc.translatePath(xbmcaddon.Addon().getAddonInfo("profile"))
+property_name = xbmcaddon.Addon().getAddonInfo("id") + ".skinpatch-status"
+window = xbmcgui.Window(10000)
+
+
+class SkinPatch():
+    def __init__(self, skin_id=None):
+        self.skin_id = skin_id or xbmc.getSkinDir()
+        self.skin_path = xbmcaddon.Addon(self.skin_id).getAddonInfo("path")
+        self.applied_patch = None
+
+        # Find patch files for the current skin
+        patch_path = os.path.join(xbmcaddon.Addon().getAddonInfo("path"), "resources", "skins")
+        self.patches = []
+        for patch_file in os.listdir(patch_path):
+            if patch_file.startswith(self.skin_id):
+                self.patches.append(os.path.join(patch_path, patch_file))
+        self.patches.sort(reverse=True)
+        self.get_status()
+
+    # Apply the patch using python-patch https://github.com/techtonik/python-patch
+    def apply(self):
+        if self.status != STATUS_CLEAN:
+            return False
+        for patch_path in self.patches:
+            patchset = patch.fromfile(patch_path)
+            if patchset.can_apply(root=self.skin_path) and patchset.apply(root=self.skin_path):
+                # Set patch status
+                self.status = STATUS_PATCHED
+                self.patch = os.path.basename(patch_path)
+                self.files = [(item.target, hash_file(os.path.join(self.skin_path, item.target)))
+                              for item in patchset.items]
+                self.version = xbmcaddon.Addon().getAddonInfo("version")
+                self.save_status()
+                # Copy patch file to allow removal later
+                copy_path = os.path.join(data_path, "skins", self.patch)
+                if not os.path.isdir(os.path.dirname(copy_path)):
+                    os.makedirs(os.path.dirname(copy_path))
+                shutil.copy(patch_path, copy_path)
+                return True
+
+    # Remove the patch using python-patch
+    def revert(self):
+        if self.status == STATUS_CLEAN:
+            return False
+        patch_path = os.path.join(data_path, "skins", self.patch)
+        patchset = patch.fromfile(patch_path)
+        if patchset.revert(root=self.skin_path):
+            self.status = STATUS_CLEAN
+            self.save_status()
+            os.remove(patch_path)
+            window.clearProperty(property_name)
+            return True
+
+    # Apply patch if required and re-apply if patch file changes
+    def autopatch(self):
+        if self.status == STATUS_UPDATED:
+            self.revert()
+        if self.status == STATUS_CLEAN:
+            if self.apply():
+                xbmc.executebuiltin("ReloadSkin()")
+            else:
+                return
+        window.setProperty(property_name, self.version)
+
+    # Load and verify skin patch info
+    def get_status(self):
+        self.status = STATUS_CLEAN
+        self.patch = None
+        self.files = None
+        self.version = None
+
+        # Retrieve last-patch information from XML file
+        xml_path = os.path.join(data_path, "skinpatch.xml")
+        if os.path.isfile(xml_path):
+            xml = et.parse(xml_path).getroot().find("skin[@id='{0}']".format(self.skin_id))
+            if xml is not None:
+                self.files = [(file.text, file.attrib["check"]) for file in xml.findall("file")]
+                self.patch = xml.find("patch").text
+                self.version = xml.find("patch").attrib["version"]
+
+        # Check if status has already been verified this session
+        # If the addon or skin are updated, the property value won't match
+        version = xbmcaddon.Addon().getAddonInfo("version")
+        if window.getProperty(property_name) == version:
+            self.status = STATUS_PATCHED
+        elif self.patch is not None:
+            # Verify patch integrity by checking all modified files
+            for path, check in self.files:
+                if hash_file(os.path.join(self.skin_path, path)) != check:
+                    break
+            else:
+                self.status = STATUS_PATCHED
+            # Check if addon (and not skin) has been updated since patching
+            if self.version != version and self.status == STATUS_PATCHED:
+                self.status = STATUS_UPDATED
+
+    # Save skin patch status to XML file
+    def save_status(self):
+        xml_path = os.path.join(data_path, "skinpatch.xml")
+        if os.path.isfile(xml_path):
+            xml = et.parse(xml_path).getroot()
+            old = xml.find("skin[@id='{0}']".format(self.skin_id))
+            if old is not None:
+                xml.remove(old)
+        else:
+            xml = et.Element("skinpatch")
+        if self.status != STATUS_CLEAN:
+            skin = et.Element("skin", attrib={"id": self.skin_id})
+            patch = et.Element("patch", attrib={"version": self.version})
+            patch.text = self.patch
+            skin.append(patch)
+            for path, check in self.files:
+                file = et.Element("file", attrib={"check": check})
+                file.text = path
+                skin.append(file)
+            xml.append(skin)
+        et.ElementTree(xml).write(xml_path, encoding="utf-8")
+
+
+def autoremove():
+    xml_path = os.path.join(data_path, "skinpatch.xml")
+    if os.path.isfile(xml_path):
+        xml = et.parse(xml_path).getroot()
+        for skin in xml.findall("skin"):
+            SkinPatch(skin.attrib["id"]).revert()
+        xbmc.executebuiltin("ReloadSkin()")
+
+
+def hash_file(path):
+    import hashlib
+    BLOCKSIZE = 65536
+    hasher = hashlib.md5()
+    with open(path, "rb") as file:
+        buf = file.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = file.read(BLOCKSIZE)
+    return hasher.hexdigest()
